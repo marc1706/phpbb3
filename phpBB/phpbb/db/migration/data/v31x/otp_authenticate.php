97e08571308980378150dfc23979c89c6fa0be53
Marc Alexander <admin@m-a-styles.de>
2015-02-08 01:56:06 +0100

    [ticket/13458] Add migration for adding otp columns to users table
    PHPBB3-13458

---
+++ phpBB/phpbb/db/migration/data/v31x/otp_authenticate.php
@@ -0,0 +1,46 @@
+<?php
+/**
+*
+* This file is part of the phpBB Forum Software package.
+*
+* @copyright (c) phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+* For full copyright and license information, please see
+* the docs/CREDITS.txt file.
+*
+*/
+
+namespace phpbb\db\migration\data\v31x;
+
+class otp_authenticate extends \phpbb\db\migration\migration
+{
+	static public function depends_on()
+	{
+		return array('\phpbb\db\migration\data\v31x\v313rc2');
+	}
+
+	public function update_schema()
+	{
+		return array(
+			'add_columns'	=> array(
+				$this->table_prefix . 'users'			=> array(
+					'user_otp_secret'		=> array('VCHAR:255', ''),
+					'user_otp_counter'		=> array('UINT', 0),
+				),
+			),
+		);
+	}
+
+	public function revert_schema()
+	{
+		return array(
+			'remove_columns'	=> array(
+				$this->table_prefix . 'users'			=> array(
+					'user_otp_secret',
+					'user_otp_counter',
+				),
+			),
+		);
+	}
+}
