35d3c6f871573e3b5e8443fcdfa806a7f1730d50
Marc Alexander <admin@m-a-styles.de>
2016-03-15 13:20:44 +0100

    [feature/acp_redesign] Extend gulp task and add colors & css files

--- phpBB/gulpfile.js
+++ phpBB/gulpfile.js
@@ -1,9 +1,10 @@
 'use strict';
 
+var del = require('del');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('adm_compile_sass', function() {
+gulp.task('adm_compile_sass', ['clean_adm'], function() {
     gulp.src('adm/style/assets/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('adm/style/assets/css'));
@@ -11,4 +12,8 @@ gulp.task('adm_compile_sass', function() {
 
 gulp.task('watch_adm', function() {
     gulp.watch('adm/style/assets/scss/*.scss',['adm_compile_sass']);
+});
+
+gulp.task('clean_adm', function () {
+    del(['adm/style/assets/css/*']);
 });
