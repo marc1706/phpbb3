8cce8b52bfbcee39f7e5cc98b8ec1b74eb9957e5
Marc Alexander <admin@m-a-styles.de>
2016-03-15 13:56:05 +0100

    [feature/acp_redesign] Compile bootstrap into adm style.css

--- phpBB/gulpfile.js
+++ phpBB/gulpfile.js
@@ -1,19 +1,33 @@
 'use strict';
 
-var del = require('del');
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var del = require('del'),
+	gulp = require('gulp'),
+	sass = require('gulp-sass');
+
+var admConfig = {
+	cssPath: './adm/style/assets/css',
+	sassPath: './adm/style/assets/scss',
+	bootstrapDir: './vendor/twbs/bootstrap-sass'
+};
 
 gulp.task('adm_compile_sass', ['clean_adm'], function() {
-    gulp.src('adm/style/assets/scss/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('adm/style/assets/css'));
+	console.log(admConfig.bootstrapDir + '/assets/stylesheets');
+	return gulp.src(admConfig.sassPath + '/style.scss')
+		.pipe(sass({
+			style: 'compressed',
+			includePaths: [
+				admConfig.sassPath,
+				admConfig.bootstrapDir + '/assets/stylesheets'
+			]
+		})
+			.on("error", sass.logError))
+		.pipe(gulp.dest(admConfig.cssPath));
 });
 
 gulp.task('watch_adm', function() {
-    gulp.watch('adm/style/assets/scss/*.scss',['adm_compile_sass']);
+	gulp.watch('adm/style/assets/scss/*.scss',['adm_compile_sass']);
 });
 
 gulp.task('clean_adm', function () {
-    del(['adm/style/assets/css/*']);
+	del(['adm/style/assets/css/*']);
 });
