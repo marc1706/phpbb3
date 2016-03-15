bcace19e4997b6645d191c16d83b9ed51cc4f2fb
Marc Alexander <admin@m-a-styles.de>
2016-03-15 16:40:41 +0100

    [feature/acp_redesign] Add more assets for bootstrap

--- phpBB/gulpfile.js
+++ phpBB/gulpfile.js
@@ -2,16 +2,24 @@
 
 var del = require('del'),
 	gulp = require('gulp'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	postcss = require('gulp-postcss'),
+	path = require('path');
 
 var admConfig = {
 	cssPath: './adm/style/assets/css',
+	fontsPath: './adm/style/assets/fonts',
+	imagesPath: './adm/style/assets/images',
+	jsPath: './adm/style/assets/js',
 	sassPath: './adm/style/assets/scss',
 	bootstrapDir: './vendor/twbs/bootstrap-sass'
 };
 
-gulp.task('adm_compile_sass', ['clean_adm'], function() {
-	console.log(admConfig.bootstrapDir + '/assets/stylesheets');
+gulp.task('adm_compile_sass', ['clean_adm', 'copy_adm_js', 'copy_adm_fonts'], function() {
+	var processors = [
+		require('autoprefixer')()
+	];
+
 	return gulp.src(admConfig.sassPath + '/style.scss')
 		.pipe(sass({
 			style: 'compressed',
@@ -19,9 +27,19 @@ gulp.task('adm_compile_sass', ['clean_adm'], function() {
 				admConfig.sassPath,
 				admConfig.bootstrapDir + '/assets/stylesheets'
 			]
-		})
-			.on("error", sass.logError))
+		}).on("error", sass.logError))
+		.pipe(postcss(processors))
 		.pipe(gulp.dest(admConfig.cssPath));
+});
+
+gulp.task('copy_adm_js', function() {
+	return gulp.src(admConfig.bootstrapDir + '/assets/javascripts/bootstrap.min.js')
+		.pipe(gulp.dest(admConfig.jsPath));
+});
+
+gulp.task('copy_adm_fonts', function () {
+	return gulp.src(admConfig.bootstrapDir + '/assets/fonts/bootstrap/*')
+		.pipe(gulp.dest(admConfig.fontsPath + '/bootstrap/'));
 });
 
 gulp.task('watch_adm', function() {
@@ -30,4 +48,5 @@ gulp.task('watch_adm', function() {
 
 gulp.task('clean_adm', function () {
 	del(['adm/style/assets/css/*']);
+	del(['adm/style/assets/js/*']);
 });
