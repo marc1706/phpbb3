90a5525694ca823c90b135beb9211fd3564f2bc4
Marc Alexander <admin@m-a-styles.de>
2020-12-01 20:30:52 +0100

    [task/github-actions] Map postgresql run folder

--- travis/setup-database.sh
+++ travis/setup-database.sh
@@ -22,9 +22,8 @@ fi
 
 if [ "$DB" == "postgres" ]
 then
-	PGPASSWORD=postgres psql -h localhost -c 'DROP DATABASE IF EXISTS phpbb_tests;' -U postgres
-	PGPASSWORD=postgres psql -h localhost -c 'create database phpbb_tests;' -U postgres
-	export DATABASE_URL="pgsql://postgres:postgres@localhost:5432/phpbb_tests"
+	psql -c 'DROP DATABASE IF EXISTS phpbb_tests;' -U postgres
+	psql -c 'create database phpbb_tests;' -U postgres
 fi
 
 if [ "$TRAVIS_PHP_VERSION" == "5.6" -a "$DB" == "mysql" ]
